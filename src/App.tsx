49b49a0f469980ffdcf0d24b3b5b80b71e04b76c
eleventh <allan@celedonio.digital>
2023-07-20 14:07:07 +1200

    Publish Base Template

---
+++ src/App.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src="/assets/images/logo.svg" className="App-logo" alt="Celedonio Digital Logo" />
+        <p>
+          This is a base react template for
+        </p>
+        <a
+          className="App-link"
+          href="https://celedonio.digital"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Celedonio Digital
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
